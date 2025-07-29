bd8d222810768d2373440f7fd56ec7e586d53142
serena koumetio <serenakoumetio@gmail.com>
2025-07-29 23:34:42 +0200

    Ajout du composant footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+   <div>
+    je suis le footer
+   </div>
+  );
+}
+
+export default Footer;
